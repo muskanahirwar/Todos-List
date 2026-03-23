d05a43d00c5da7cd6f354dff14c32d521b309ef4
Muskan Ahirwar <103559819+muskanahirwar@users.noreply.github.com>
2026-03-23 21:23:37 +0530

    Initial commit: React Todo List app with add/edit/delete todos, enhanced README

---
+++ TODO.md
@@ -0,0 +1,1 @@
+# GitHub Share Progress\n\n- [ ] 1. Update README.md\n- [ ] 2. git add/commit/push\n- [ ] 3. Build app\n- [ ] 4. Complete\n
---
+++ src/MyComponents/About.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+        This is an about component. 
+        <p>Here we will write about our website and the purpose of creating this website. 
+            This is a simple todo list app created using React.js. It is a single page application
+             which allows users to add, delete and view their todos.
+             The todos are stored in the local storage of the browser. 
+        </p>
+    </div>
+  )
+}
+
+export default About
